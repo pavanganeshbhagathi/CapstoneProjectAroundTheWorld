a3ef59bb59c74fab9d95a3edffbfeb8300c08301
YashasreeTummala <yashasreeaditya@gmail.com>
2020-07-25 20:19:37 +0530

    80% completed

--- travellerfrontend/traveller/src/app/components/landingpage/landingpage.component.ts
+++ travellerfrontend/traveller/src/app/components/landingpage/landingpage.component.ts
@@ -10,56 +10,89 @@ export class LandingpageComponent implements OnInit {
 constructor(){
 }
 
-  imageObject: Array<object> = [
+  imageObject1: Array<object> = [
     {
-      image: '../../../assets/11.jpg', // Support base64 image
-      thumbImage: '../../../assets/img/1 (1).jpg',
-      alt: '', // Optional: You can use this key if want to show image with alt
+      image: '../../../assets/slider/1.jpg', // Support base64 image
+      thumbImage: '../../../assets/slider/1.jpg',
+      alt: 'SUMMER', // Optional: You can use this key if want to show image with alt
     },
     {
-      image: '../../../assets/22.jpg',
-      thumbImage: '../../../assets/img/2 (1).jpg',
+      image: '../../../assets/slider/Honeymoon-Packages-for-Goa.jpg',
+      thumbImage: '../../../assets/slider/Honeymoon-Packages-for-Goa.jpg',
       alt: '',
     },
     {
-      image: '../../../assets/33.jpg', // Support base64 image
-      thumbImage: '../../../assets/img/3.jpg',
+      image: '../../../assets/slider/reasons-to-visit-romania.jpg', // Support base64 image
+      thumbImage: '../../../assets/slider/reasons-to-visit-romania.jpg',
       alt: '', // Optional: You can use this key if want to show image with alt
     },
     {
-      image: '../../../assets/44.jpg',
-      thumbImage: '../../../assets/img/4.jpg',
+      image: '../../../assets/slider/Konark-Sun-Temple.jpg',
+      thumbImage: '../../../assets/slider/Konark-Sun-Temple.jpg',
       alt: '',
     },
     {
-      image: '../../../assets/55.jpg', // Support base64 image
-      thumbImage: '../../../assets/img/1 (4).jpg',
+      image: '../../../assets/slider/historical-tajmahal.jpg', // Support base64 image
+      thumbImage: '../../../assets/slider/historical-tajmahal.jpg',
       alt: '', // Optional: You can use this key if want to show image with alt
     },
     {
-      image: '../../../assets/66.jpg', // Support base64 image
-      thumbImage: '../../../assets/img/1 (5).jpg',
+      image: '../../../assets/slider/FEATURE_FO_DOMINO_PARK_08_THE_LAWN_BARRETT_DOHERTY.jpg', // Support base64 image
+      thumbImage: '../../../assets/slider/FEATURE_FO_DOMINO_PARK_08_THE_LAWN_BARRETT_DOHERTY.jpg',
       alt: '', // Optional: You can use this key if want to show image with alt
     },
     {
-      image: '../../../assets/77.jpg', // Support base64 image
-      thumbImage: '../../../assets/img/1 (6).jpg',
+      image: '../../../assets/slider/island.jpg', // Support base64 image
+      thumbImage: '../../../assets/slider/island.jpg',
       alt: '', // Optional: You can use this key if want to show image with alt
     },
     {
-      image: '../../../assets/88.jpg', // Support base64 image
-      thumbImage: '../../../assets/img/2 (5).jpg',
+      image: '../../../assets/slider/Historical-Places-in-Central-India-that-You-Must-Visit.jpg', // Support base64 image
+      thumbImage: '../../../assets/slider/Historical-Places-in-Central-India-that-You-Must-Visit.jpg',
       alt: '', // Optional: You can use this key if want to show image with alt
     },
     {
-      image: '../../../assets/99.jpg', // Support base64 image
-      thumbImage: '../../../assets/img/1 (2).jpg',
+      image: '../../../assets/slider/Baga-Beach-in-goa.webp', // Support base64 image
+      thumbImage: '../../../assets/slider/Baga-Beach-in-goa.webp',
       alt: '', // Optional: You can use this key if want to show image with alt
+    },
+    {
+      image: '../../../assets/slider/190410094953-india-waterfalls---athirappalli-waterfalls-full-169.jpg', // Support base64 image
+      thumbImage: '../../../assets/slider/190410094953-india-waterfalls---athirappalli-waterfalls-full-169.jpg',
+      alt: '', // Optional: You can use this key if want to show image with alt
+    }
+  ];
+
+  imageObject2: Array<object> = [
+    {
+      image: '../../../assets/slider/XSeJqH.jpg', // Support base64 image
+      thumbImage: '../../../assets/slider/XSeJqH.jpg',
+      alt: '', // Optional: You can use this key if want to show image with alt
+    },
+    {
+      image: '../../../assets/slider/9ce283829510f3853492e725f094c694.jpg',
+      thumbImage: '../../../assets/slider/9ce283829510f3853492e725f094c694.jpg',
+      alt: '',
+    },
+    {
+      image: '../../../assets/slider/indian-monsoon-and-its-impact.webp', // Support base64 image
+      thumbImage: '../../../assets/slider/indian-monsoon-and-its-impact.webp',
+      alt: '', // Optional: You can use this key if want to show image with alt
+    },
+    {
+      image: '../../../assets/slider/msid-65105346,width-96,height-65.cms.webp',
+      thumbImage: '../../../assets/slider/msid-65105346,width-96,height-65.cms.webp',
+      alt: '',
+    },
+    {
+      image: '../../../assets/slider/Sinhagad-Fort-during-monsoons-.webp',
+      thumbImage: '../../../assets/slider/Sinhagad-Fort-during-monsoons-.webp',
+      alt: '',
     }
   ];
   sliderInfinite = true;
   sliderAutoSlide = true;
-  sliderAnimationSpeed = 3;
+  sliderAnimationSpeed = -5;
 
   ngOnInit(): void { }
 
--- travellerfrontend/traveller/src/app/components/loginpage/loginpage.component.ts
+++ travellerfrontend/traveller/src/app/components/loginpage/loginpage.component.ts
@@ -22,54 +22,29 @@ export class LoginpageComponent implements OnInit {
 
   imageObject: Array<object> = [
     {
-      image: '../../../assets/11.jpg', // Support base64 image
-      thumbImage: '../../../assets/img/1 (1).jpg',
+      image: '../../../assets/o0.jpg', // Support base64 image
+      thumbImage: '../../../assets/o0.jpg',
       alt: '', // Optional: You can use this key if want to show image with alt
     },
     {
-      image: '../../../assets/22.jpg',
-      thumbImage: '../../../assets/img/2 (1).jpg',
+      image: '../../../assets/o3.jpg',
+      thumbImage: '../../../assets/o3.jpg',
       alt: '',
     },
     {
-      image: '../../../assets/33.jpg', // Support base64 image
-      thumbImage: '../../../assets/img/3.jpg',
-      alt: '', // Optional: You can use this key if want to show image with alt
-    },
-    {
-      image: '../../../assets/44.jpg',
-      thumbImage: '../../../assets/img/4.jpg',
+      image: '../../../assets/o7.jpg',
+      thumbImage: '../../../assets/o7.png',
       alt: '',
     },
     {
-      image: '../../../assets/55.jpg', // Support base64 image
-      thumbImage: '../../../assets/img/1 (4).jpg',
+      image: '../../../assets/o6.jpg', // Support base64 image
+      thumbImage: '../../../assets/o6.jpg',
       alt: '', // Optional: You can use this key if want to show image with alt
-    },
-    {
-      image: '../../../assets/66.jpg', // Support base64 image
-      thumbImage: '../../../assets/img/1 (5).jpg',
-      alt: '', // Optional: You can use this key if want to show image with alt
-    },
-    {
-      image: '../../../assets/77.jpg', // Support base64 image
-      thumbImage: '../../../assets/img/1 (6).jpg',
-      alt: '', // Optional: You can use this key if want to show image with alt
-    },
-    {
-      image: '../../../assets/88.jpg', // Support base64 image
-      thumbImage: '../../../assets/img/2 (5).jpg',
-      alt: '', // Optional: You can use this key if want to show image with alt
-    },
-    {
-      image: '../../../assets/99.jpg', // Support base64 image
-      thumbImage: '../../../assets/img/1 (2).jpg',
-      alt: '', // Optional: You can use this key if want to show image with alt
-    },
+    }
   ];
   sliderInfinite = true;
   sliderAutoSlide = true;
-  sliderAnimationSpeed = 3;
+  sliderAnimationSpeed = 13;
 
   ngOnInit(): void {
     this.authService.authState.subscribe((user) => {
